7f919f4bbce468cbb1035e0808bfe601fcbe5669
zhou <774141367@qq.com>
2021-10-29 15:19:53 +0800

    Request加入set(setHeader())

--- packages/utils/lib/src/webapi.js
+++ packages/utils/lib/src/webapi.js
@@ -24,6 +24,28 @@ export const buildRoute = (url) => {
     return `${url}${connector}token=${user.token}`;
 };
 
+const setHeader = () => {
+    const user = JSON.parse(sessionStorage.getItem('user'));
+    if (user == null) {
+        const token = getToken();
+        if (token) {
+            return {
+                token: token
+            };
+        } else
+            return {};
+    } else {
+        return {
+            token: user.token
+        };
+    }
+};
+
+const getToken = () => {
+    let search = window.location.search
+    let hasToken = search.includes('token')
+    return hasToken ? search.match(/token=(.*)/)[1] : null
+}
 const resultHandler =
     (resolve, reject) =>
         (err, res) => {
@@ -32,7 +54,7 @@ const resultHandler =
                     // 退出到登录页
                     const user = JSON.parse(sessionStorage.getItem('user'));
                     sessionStorage.removeItem('user');
-                    if (user.domain) {
+                    if (user && user.domain) {
                         window.document.location.replace(`/${user.domain}/signin`)
                     } else {
                         window.document.location.replace('/signin');
@@ -48,28 +70,32 @@ const resultHandler =
 
 export class Request {
     static get = (url, query) => new Promise((resolve, reject) => {
-        request.get(buildUrl(url)).query(query).use(noCache).end(resultHandler(resolve, reject));
+        request.get(buildUrl(url)).set(setHeader()).query(query).use(noCache).end(resultHandler(resolve, reject));
     });
 
     static post = (url, data, query) => new Promise((resolve, reject) => {
-        request.post(buildUrl(url)).query(query).use(noCache).send(data).end(resultHandler(resolve, reject));
+        request.post(buildUrl(url)).set(setHeader()).query(query).use(noCache).send(data).end(resultHandler(resolve, reject));
     });
 
     static put = (url, data) => new Promise((resolve, reject) => {
-        request.put(buildUrl(url)).send(data).use(noCache).end(resultHandler(resolve, reject));
+        request.put(buildUrl(url)).set(setHeader()).send(data).use(noCache).end(resultHandler(resolve, reject));
     });
 
     static delete = url => new Promise((resolve, reject) => {
-        request.delete(buildUrl(url)).use(noCache).end(resultHandler(resolve, reject));
+        request.delete(buildUrl(url)).set(setHeader()).use(noCache).end(resultHandler(resolve, reject));
     });
 }
 
 export class RouteRequest {
     static get = url => new Promise((resolve, reject) => {
-        request.get(buildRoute(url)).end(resultHandler(resolve, reject));
+        request.get(buildRoute(url)).set(setHeader()).end(resultHandler(resolve, reject));
     });
 
-    static post = (url, data) => new Promise((resolve, reject) => {
-        request.post(buildRoute(url)).send(data).end(resultHandler(resolve, reject));
+    static post = (url, data, query) => new Promise((resolve, reject) => {
+        request.post(buildRoute(url)).set(setHeader()).query(query).send(data).end(resultHandler(resolve, reject));
+    });
+
+    static delete = (url, data, query) => new Promise((resolve, reject) => {
+        request.delete(buildRoute(url)).set(setHeader()).query(query).send(data).end(resultHandler(resolve, reject));
     });
 }
